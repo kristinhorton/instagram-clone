65d475f2511ef299ae2c91398bbe30f290b7f139
Kristin Horton <khorton94@gmail.com>
2024-01-15 11:19:15 -0500

    update homepage to show suggested users when screen is lg and above

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ export const HomePage = () => {
         <Box flex={2} py={10}>
           <FeedPosts />
         </Box>
-        <Box flex={3} mr={20} display={{ base: 'none', xl: 'block' }} maxW='300px'>
+        <Box flex={3} mr={20} display={{ base: 'none', lg: 'block' }} maxW='300px'>
           <SuggestedUsers />
         </Box>
       </Flex>
